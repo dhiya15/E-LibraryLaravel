b90dbc1a55acb95295d4bb1b3da104fce496bf8b
dhiya.edd <abid.dhiyaedd@gmail.com>
2022-10-11 23:09:48 +0100

    commit

--- app/Http/Controllers/LivreController.php
+++ app/Http/Controllers/LivreController.php
@@ -4,43 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LivreRequest;
 use App\Models\Livre;
+use Illuminate\Http\Request;
 
 class LivreController extends Controller
 {
 
-    protected $count = 0;
-
-    public function trouverLivre(LivreRequest $request)
+    public function trouverLivre(Request $request)
     {
         try {
-            $data = $request->validated();
-            $livres = Livre::query();
-            if(isset($data["titres"])){
-                $livres = $livres->where("titres", "like", "%". $data["titres"] ."%");
-                $this->count += 1;
-            }
-            $livres = $this->checkIfIsSet($data, $livres, "cotation");
-            $livres = $this->checkIfIsSet($data, $livres, "auteur");
-            $livres = $this->checkIfIsSet($data, $livres, "inventaire");
-            $livres = $this->checkIfIsSet($data, $livres, "spécialiste");
-            $livres = $this->checkIfIsSet($data, $livres, "date_edition");
-            $livres = $this->checkIfIsSet($data, $livres, "editeur");
-            $livres = $this->checkIfIsSet($data, $livres, "isbn");
-            $livres = $livres->get();
-            if (is_null($livres) || $this->count == 0) {
+            $data = $request->all();
+            $livres = Livre::query()
+                ->where("titres", "like", "%". $data["keyword"] ."%")
+                ->orWhere("cotation", "like", "%". $data["keyword"] ."%")
+                ->orWhere("auteur", "like", "%". $data["keyword"] ."%")
+                ->orWhere("inventaire", "like", "%". $data["keyword"] ."%")
+                ->orWhere("spécialiste", "like", "%". $data["keyword"] ."%")
+                ->orWhere("date_edition", "like", "%". $data["keyword"] ."%")
+                ->orWhere("editeur", "like", "%". $data["keyword"] ."%")
+                ->orWhere("isbn", "like", "%". $data["keyword"] ."%")
+                ->get();
+            if (is_null($livres)) {
                 return response()->json([], 404);
             }
             return response()->json($livres);
         }catch (\Exception $e){
-            dd($e->getMessage());
+            return response()->json([], 404);
         }
-    }
-
-    public function checkIfIsSet($all, $objectQuery, $paramName) {
-        if(isset($all[$paramName])) {
-            $objectQuery = $objectQuery->orWhere("$paramName", "like", "%" . $all["$paramName"] . "%");
-            $this->count += 1;
-        }
-        return $objectQuery;
     }
 }
